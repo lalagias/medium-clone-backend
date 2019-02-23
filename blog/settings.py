8c4775879907d5c5f6188acf0466c91c5ce6733a
lalagias <jimkountanis@gmail.com>
2019-02-23 19:23:10 +0200

    change debug mode

--- blog/settings.py
+++ blog/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '*9iou4)*spksc78rg&)2v8=q_x-kmis4e0l$_&iz1*idaou@d3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
